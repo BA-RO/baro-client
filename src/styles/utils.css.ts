a10d9af00544829917a95ace65521ffc451b39d1
cje <chej098t@gmail.com>
2023-12-08 02:49:54 +0900

    feat: visuallyHidden util 추가

--- src/styles/utils.css.ts
+++ src/styles/utils.css.ts
@@ -15,6 +15,18 @@ export const truncate = style({
   whiteSpace: 'nowrap',
 });
 
+export const visuallyHidden = style({
+  position: 'absolute',
+  width: '1px',
+  height: '1px',
+  padding: 0,
+  margin: '-1px',
+  overflow: 'hidden',
+  clip: 'rect(0, 0, 0, 0)',
+  whiteSpace: 'nowrap',
+  border: 0,
+});
+
 export const backLayer = style({ zIndex: -1 });
 export const baseLayer = style({ zIndex: 0 });
 export const middleLayer = style({ zIndex: 50 });
