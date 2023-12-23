7a5bad411614a431156ffe44a506a6a6cecf8acf
dongkyun-dev <thd6576@naver.com>
2023-12-24 00:53:19 +0900

    feat: 폰트와 color에 대한 디자인 시스템 반영

--- src/styles/tokens.ts
+++ src/styles/tokens.ts
@@ -191,3 +191,163 @@ export const GRID_CONTENT_ALIGNMENT = {
 export const GRID_ITEMS_ALIGNMENT = ['stretch', 'start', 'center', 'end'];
 
 export const GRID_CELL = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+export const FONT = {
+  pretendard:
+    '"Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif',
+};
+
+export const FONT_SIZE = {
+  '40/Title/Bold': {
+    fontSize: '40px',
+    fontWeight: FONT_WEIGHT.bold,
+    lineHeight: '48px',
+    letterSpacing: '-0.5px',
+  },
+  '40/Title/Medium': {
+    fontSize: '40px',
+    fontWeight: FONT_WEIGHT.medium,
+    lineHeight: '48px',
+    letterSpacing: '-0.5px',
+  },
+  '20/Title/Semibold': {
+    fontSize: '20px',
+    fontWeight: FONT_WEIGHT.semiBold,
+    lineHeight: '24px',
+    letterSpacing: '-0.5px',
+  },
+  '18/Body/Medium': {
+    fontSize: '18px',
+    fontWeight: FONT_WEIGHT.medium,
+    lineHeight: '28px',
+    letterSpacing: '-0.3px',
+  },
+  '18/Body/Regular': {
+    fontSize: '18px',
+    fontWeight: FONT_WEIGHT.regular,
+    lineHeight: '28px',
+    letterSpacing: '-0.3px',
+  },
+  '17/Body/Medium': {
+    fontSize: '17px',
+    fontWeight: FONT_WEIGHT.medium,
+    lineHeight: '27px',
+    letterSpacing: '-0.3px',
+  },
+  '17/Body/Regular': {
+    fontSize: '17px',
+    fontWeight: FONT_WEIGHT.regular,
+    lineHeight: '27px',
+    letterSpacing: '-0.3px',
+  },
+  '16/Title/Bold': {
+    fontSize: '16px',
+    fontWeight: FONT_WEIGHT.bold,
+    lineHeight: '19px',
+    letterSpacing: '-0.5px',
+  },
+  '16/Title/Medium': {
+    fontSize: '16px',
+    fontWeight: FONT_WEIGHT.medium,
+    lineHeight: '19px',
+    letterSpacing: '-0.5px',
+  },
+  '16/Body/Medium': {
+    fontSize: '16px',
+    fontWeight: FONT_WEIGHT.medium,
+    lineHeight: '25px',
+    letterSpacing: '-0.3px',
+  },
+  '16/Body/Regular': {
+    fontSize: '16px',
+    fontWeight: FONT_WEIGHT.regular,
+    lineHeight: '25px',
+    letterSpacing: '-0.3px',
+  },
+  '15/Title/Bold': {
+    fontSize: '15px',
+    fontWeight: FONT_WEIGHT.bold,
+    lineHeight: '18px',
+    letterSpacing: '0px',
+  },
+  '15/Title/Medium': {
+    fontSize: '15px',
+    fontWeight: FONT_WEIGHT.medium,
+    lineHeight: '18px',
+    letterSpacing: '0px',
+  },
+  '15/Body/Medium': {
+    fontSize: '15px',
+    fontWeight: FONT_WEIGHT.medium,
+    lineHeight: '24px',
+    letterSpacing: '-0.2px',
+  },
+  '15/Body/Regular': {
+    fontSize: '15px',
+    fontWeight: FONT_WEIGHT.regular,
+    lineHeight: '24px',
+    letterSpacing: '-0.2px',
+  },
+  '14/Title/Semibold': {
+    fontSize: '14px',
+    fontWeight: FONT_WEIGHT.semiBold,
+    lineHeight: '18px',
+    letterSpacing: '-0.5px',
+  },
+  '14/Body/Regular': {
+    fontSize: '14px',
+    fontWeight: FONT_WEIGHT.regular,
+    lineHeight: '18px',
+    letterSpacing: '-0.3px',
+  },
+  '14/Caption/Medium': {
+    fontSize: '14px',
+    fontWeight: FONT_WEIGHT.medium,
+    lineHeight: '18px',
+    letterSpacing: '0px',
+  },
+  '14/Caption/Regular': {
+    fontSize: '14px',
+    fontWeight: FONT_WEIGHT.regular,
+    lineHeight: '18px',
+    letterSpacing: '0px',
+  },
+  '13/Body/Regular': {
+    fontSize: '13px',
+    fontWeight: FONT_WEIGHT.regular,
+    lineHeight: '17px',
+    letterSpacing: '-0.3px',
+  },
+  '11/Caption/Medium': {
+    fontSize: '11px',
+    fontWeight: FONT_WEIGHT.medium,
+    lineHeight: '14px',
+    letterSpacing: '0px',
+  },
+};
+
+export const COLORS = {
+  'Grey/White': '#ffffff',
+  'Grey/100': '#f4f5f8',
+  'Grey/150': '#eceef3',
+  'Grey/200': '#e3e5eb',
+  'Grey/250': '#d1d2d7',
+  'Grey/300': '#a8aaad',
+  'Grey/400': '#808285',
+  'Grey/500': '#545454',
+  'Grey/600': '#2e2e2e',
+  'Grey/700': '#232323',
+  'Grey/800': '#1c1c1c',
+  'Grey/900': '#121212',
+  'Dim/6': 'rgba(0, 0, 0, 0.06)',
+  'Dim/50': 'rgba(0, 0, 0, 0.5)',
+  'Dim/70': 'rgba(0, 0, 0, 0.7)',
+  'Dim/85': 'rgba(0, 0, 0, 0.85)',
+  'Blue/Default': '#4c94ff',
+  'Blue/Light': '#eff5ff',
+  'Blue/Dark': '#327ceb',
+  'Blue/Gradient': '#4C94FF',
+  Green: '#33d486',
+  Yellow: '#ffc839',
+  Red: '#f7463b',
+};
