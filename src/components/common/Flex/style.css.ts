83d386be2e34227746bd301288a368841cb15ca5
dongkyun-dev <thd6576@naver.com>
2023-12-24 00:51:31 +0900

    feat: grid, flex에서 style 관련된 요소는 모두 className을 통해 들어오도록 변경

---
+++ src/components/common/Flex/style.css.ts
@@ -0,0 +1,11 @@
+import { style } from '@vanilla-extract/css';
+
+export const flex = style({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  rowGap: '10px',
+  columnGap: '10px',
+  margin: '0 80px',
+  padding: '10px',
+});
