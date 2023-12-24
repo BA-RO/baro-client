1035aa0701ea7ed827e797bc4833fc26764b4b35
dongkyun-dev <thd6576@naver.com>
2023-12-24 12:33:19 +0900

    feat: Toast 테스트 환경 구축

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,20 @@
 import type { NextPage } from 'next';
 
+import { useToastStore } from '@/src/stores/toastStore';
+
 const HomePage: NextPage = () => {
-  return <>바로</>;
+  const showToast = useToastStore((state) => state.showToast);
+
+  return (
+    <div>
+      <button
+        type="button"
+        onClick={() => showToast({ message: '토스트 노출' })}
+      >
+        토스트 노출
+      </button>
+    </div>
+  );
 };
 
 export default HomePage;
