2f8585110c8cb71340ae662675b94cd32ba1bda8
dongkyun-dev <thd6576@naver.com>
2023-12-28 15:39:50 +0900

    feat: toast story 작성

--- src/components/Toast/Toast.stories.tsx
+++ src/components/Toast/Toast.stories.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 
+import { TOAST_DURATION_TIME } from '@/src/models/toastModel';
 import { useToast } from '@/src/stores/toastStore';
 
 import Toast from './Toast';
@@ -13,20 +14,40 @@ const meta: Meta<typeof Toast> = {
 export default meta;
 type Story = StoryObj<typeof Toast>;
 
-export const Default: Story = {
+export const 단순확인: Story = {
   render: function Render() {
     const { showToast } = useToast();
 
     useEffect(() => {
-      showToast({ message: '테스트' });
+      showToast({ message: '테스트', type: TOAST_DURATION_TIME.SHOW });
 
       const interval = setInterval(
-        () => showToast({ message: '테스트' }),
-        4000,
+        () => showToast({ message: '테스트', type: TOAST_DURATION_TIME.SHOW }),
+        TOAST_DURATION_TIME.SHOW + 1000,
       );
 
       return () => clearInterval(interval);
-    }, []);
+    }, [showToast]);
+
+    return <Toast />;
+  },
+};
+
+export const 액션유도: Story = {
+  render: function Render() {
+    const { showToast } = useToast();
+
+    useEffect(() => {
+      showToast({ message: '테스트', type: TOAST_DURATION_TIME.ACTION });
+
+      const interval = setInterval(
+        () =>
+          showToast({ message: '테스트', type: TOAST_DURATION_TIME.ACTION }),
+        TOAST_DURATION_TIME.ACTION + 1000,
+      );
+
+      return () => clearInterval(interval);
+    }, [showToast]);
 
     return <Toast />;
   },
