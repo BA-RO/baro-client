8d1b27354962c74ff31bb5e5145772f891866b1c
cje <chej098t@gmail.com>
2024-01-01 21:52:03 +0900

    refactor(Tabs): 스타일 토큰 상수 적용

--- src/components/Tabs/style.css.ts
+++ src/components/Tabs/style.css.ts
@@ -1,20 +1,24 @@
-import * as utils from '@/src/styles/utils.css';
 import { style } from '@vanilla-extract/css';
 import { recipe } from '@vanilla-extract/recipes';
+
+import { COLORS } from '@/src/styles/tokens';
+import * as utils from '@/src/styles/utils.css';
 
 export const tabsList = style([
   utils.flexCenter,
   {
     padding: '16px 0 12px',
     gap: '40px',
-    backgroundColor: '#ffffff',
+    backgroundColor: COLORS['Grey/White'],
   },
 ]);
 
 export const tabsTrigger = recipe({
   base: {
-    fontSize: '16px',
-    letterSpacing: '-0.5px',
+    fontSize: '15px',
+    fontWeight: 500,
+    lineHeight: '18px',
+    letterSpacing: '0px',
     paddingBottom: '4px',
     borderBottom: '2px solid transparent',
     transition: 'all 150ms ease-in-out',
