b9a69c19b14656ffb9192427d300cd47cea11226
cje <chej098t@gmail.com>
2024-01-06 16:52:29 +0900

    feat: tooltip 컴포넌트 스토리북 추가

---
+++ src/components/Tooltip/Tooltip.stories.tsx
@@ -0,0 +1,90 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Tooltip from './Tooltip';
+
+const COMPONENT_DESCRIPTION = `
+  - \`<Tooltip />\`: 모든 컴포넌트에 대한 컨텍스트와 상태를 제공합니다.
+  - \`<Tooltip.Trigger />\`: \`<Tabs.Content />\` 컴포넌트를 활성화하는 컴포넌트입니다. 사용자가 이 컴포넌트 위로 마우스를 올릴 때 Tooltip이 나타납니다.
+  - \`<Tooltip.Content />\`: Tooltip에 표시되는 내용을 담당하는 컴포넌트입니다.
+`;
+
+const meta: Meta<typeof Tooltip> = {
+  title: 'Components/Tooltip',
+  component: Tooltip,
+  parameters: {
+    componentSubtitle:
+      '특정 사용자 인터페이스에 대한 보조 설명을 제공하는 컴포넌트',
+    docs: {
+      description: {
+        component: COMPONENT_DESCRIPTION,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          gap: '200px',
+          padding: '60px 0',
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Tooltip>;
+
+export const Basic: Story = {
+  render: () => (
+    <>
+      <Tooltip>
+        <Tooltip.Trigger>
+          <div>Minimal</div>
+        </Tooltip.Trigger>
+        <Tooltip.Content>Minimal Tooltip 설명</Tooltip.Content>
+      </Tooltip>
+      <Tooltip hasArrow>
+        <Tooltip.Trigger>
+          <div>Highlight</div>
+        </Tooltip.Trigger>
+        <Tooltip.Content>Highlight Tooltip 설명</Tooltip.Content>
+      </Tooltip>
+    </>
+  ),
+};
+
+export const Minimal: Story = {
+  args: {
+    children: 'Minimal',
+    hasArrow: false,
+  },
+  render: (args) => (
+    <Tooltip hasArrow={args.hasArrow}>
+      <Tooltip.Trigger>
+        <div>{args.children}</div>
+      </Tooltip.Trigger>
+      <Tooltip.Content>Minimal Tooltip 설명</Tooltip.Content>
+    </Tooltip>
+  ),
+};
+
+export const Highlight: Story = {
+  args: {
+    children: 'Highlight',
+    hasArrow: true,
+  },
+  render: (args) => (
+    <Tooltip hasArrow={args.hasArrow}>
+      <Tooltip.Trigger>
+        <div>{args.children}</div>
+      </Tooltip.Trigger>
+      <Tooltip.Content>Highlight Tooltip 설명</Tooltip.Content>
+    </Tooltip>
+  ),
+};
