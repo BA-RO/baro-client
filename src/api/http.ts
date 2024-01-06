ee2cb45ef3e5372a770faf160f3b520a8675f632
wonjin-dev <fsdjin159@gmail.com>
2024-01-06 15:39:46 +0900

    feat: 프로젝트 공통 에러 처리

--- src/api/http.ts
+++ src/api/http.ts
@@ -1,10 +1,37 @@
-import type { AxiosRequestConfig, AxiosResponse } from 'axios';
+import type { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
 import axios from 'axios';
 
 const instance = axios.create({
   baseURL: 'https://dev.api.ba-ro.co.kr/',
   headers: { 'content-type': 'application/json' },
 });
+
+type BaroError = {
+  status?: number;
+  message?: string;
+};
+
+instance.interceptors.response.use(
+  (response: AxiosResponse) => {
+    return response;
+  },
+  (error: AxiosError<Error>) => {
+    // Network Error 발생 캐치
+    if (!error.response) {
+      return Promise.reject<BaroError>({
+        status: 408,
+        message: '현재 네트워크 상태가 불안정합니다. 잠시후 다시 시도해주세요',
+      });
+    }
+
+    // 서버 에러 캐치
+    if (error.status > 500) {
+      return Promise.reject<BaroError>({ status: error.status, message: '' });
+    }
+
+    return Promise.reject<BaroError>(error);
+  },
+);
 
 export const http = {
   get: <ResponseType>(url: string): Promise<AxiosResponse<ResponseType>> =>
