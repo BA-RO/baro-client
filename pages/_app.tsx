cad175257d1acae72eed542c529fa786c9fc0a43
dongkyun-dev <thd6576@naver.com>
2024-01-08 00:16:10 +0900

    feat: 컨벤션 변경에 따른 파일명 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from 'next/app';
 import '@/src/styles/global.css';
 
 import Layout from '@/src/components/Layout/Layout';
-import Modal from '@/src/components/Modal/Modal';
+import Modal from '@/src/components/Modal';
 import TanstackQueryProvider from '@/src/components/Providers/TanstackQueryProvider';
 import Toast from '@/src/components/Toast/Toast';
 import { pretendard } from '@/src/styles/font';
