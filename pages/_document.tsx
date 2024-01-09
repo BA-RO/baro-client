e5a0ed83c4248ca006612dc33632633b0129b9ae
cje <chej098t@gmail.com>
2024-01-09 19:18:33 +0900

    feat: portal 컴포넌트 구현

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,15 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 
+import { PORTAL_ID } from '@/src/constants/portalId';
+
 const Document = () => {
   return (
     <Html lang="ko">
       <Head />
       <body>
         <Main />
+        <div id={PORTAL_ID['TOOLTIP']} />
+        <div id={PORTAL_ID['MODAL']} />
         <NextScript />
       </body>
     </Html>
---
+++ src/components/Portal/Portal.tsx
@@ -0,0 +1,17 @@
+import type { ReactNode } from 'react';
+import ReactDOM from 'react-dom';
+
+import type { PORTAL_ID } from '@/src/constants/portalId';
+
+interface PortalProps {
+  children: ReactNode;
+  id: (typeof PORTAL_ID)[keyof typeof PORTAL_ID];
+}
+
+const Portal = ({ children, id }: PortalProps) => {
+  const root = document.getElementById(id);
+
+  return ReactDOM.createPortal(children, root);
+};
+
+export default Portal;
