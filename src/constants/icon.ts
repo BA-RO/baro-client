9b7ba6cf6a273a1a0ad4e2650320a7326e6bb1a3
dongkyun-dev <thd6576@naver.com>
2024-01-13 13:19:56 +0900

    feat: close svg icon 적용

--- src/constants/icon.ts
+++ src/constants/icon.ts
@@ -1,9 +1,11 @@
+import Close from '@assets/icons/close.svg';
 import Profle from '@assets/icons/profile.svg';
 import Submit from '@assets/icons/submit.svg';
 
 export const iconFactory = {
   profile: Profle,
   submit: Submit,
+  close: Close,
 };
 
 export type Icons = keyof typeof iconFactory;
