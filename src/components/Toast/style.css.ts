8c59dbc35c731069ea12fde29575dc8d79d06d58
dongkyun-dev <thd6576@naver.com>
2024-01-14 17:03:39 +0900

    feat: keyframe 형태로 변경

--- src/components/Toast/style.css.ts
+++ src/components/Toast/style.css.ts
@@ -1,36 +1,52 @@
+import { keyframes, style } from '@vanilla-extract/css';
 import { recipe } from '@vanilla-extract/recipes';
 
+import { TOAST_TRANSITION_DURATION } from '@constants/toast';
 import { sprinkles } from '@styles/sprinkles.css';
 import { COLORS } from '@styles/tokens';
 import { modalLayer } from '@styles/utils.css';
 
+const fadeIn = keyframes({
+  from: { bottom: 0, opacity: 0 },
+  to: { bottom: 40, opacity: 1 },
+});
+
+const fadeOut = keyframes({
+  from: { bottom: 40, opacity: 1 },
+  to: { bottom: 0, opacity: 0 },
+});
+
 export const toast = recipe({
   base: [
     modalLayer,
-    sprinkles({
-      typography: '14/Body/Regular',
-    }),
     {
-      backgroundColor: COLORS['Dim/70'],
+      backgroundColor: COLORS['Dim/85'],
       position: 'fixed',
       color: COLORS['Grey/White'],
       bottom: 0,
       left: '50%',
-      transform: 'translateX(-50%) translateY(30px)',
-      borderRadius: '6px',
+      transform: 'translateX(-50%)',
+      minWidth: 100,
+      borderRadius: 12,
       textAlign: 'center',
       padding: '23px 32px',
-      transition: 'transform 400ms ease, opacity 400ms',
-      opacity: 0,
       pointerEvents: 'none',
     },
   ],
   variants: {
     isActive: {
       true: {
-        opacity: 1,
-        transform: 'translateX(-50%) translateY(-30px)',
+        animation: `${fadeIn} ${TOAST_TRANSITION_DURATION}ms forwards`,
+      },
+      false: {
+        animation: `${fadeOut} ${TOAST_TRANSITION_DURATION}ms forwards`,
       },
     },
   },
 });
+
+export const toastText = style([
+  sprinkles({
+    typography: '14/Body/Regular',
+  }),
+]);
