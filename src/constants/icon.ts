75dae17711ecee917a81c479f1659c8a4e126446
dongkyun-dev <thd6576@naver.com>
2024-01-16 23:45:11 +0900

    feat: icon 추가

--- src/constants/icon.ts
+++ src/constants/icon.ts
@@ -1,16 +1,24 @@
+import Add from '@assets/icons/add.svg';
 import Archive from '@assets/icons/archive.svg';
 import Close from '@assets/icons/close.svg';
 import Copy from '@assets/icons/copy.svg';
+import Logout from '@assets/icons/logout.svg';
 import Menu from '@assets/icons/menu.svg';
 import Profle from '@assets/icons/profile.svg';
+import ProfileDialog from '@assets/icons/profileDialog.svg';
+import Setting from '@assets/icons/setting.svg';
 import Submit from '@assets/icons/submit.svg';
 
 export const iconFactory = {
+  add: Add,
   archive: Archive,
   close: Close,
   copy: Copy,
+  logout: Logout,
   menu: Menu,
   profile: Profle,
+  profileDialog: ProfileDialog,
+  setting: Setting,
   submit: Submit,
 };
 
