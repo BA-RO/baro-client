7ba314bb2d49c1ea902c7c2d8f896a39b007a04e
cje <chej098t@gmail.com>
2024-01-17 05:12:26 +0900

    chore(assets): 아이콘 추가

--- src/constants/icon.ts
+++ src/constants/icon.ts
@@ -1,11 +1,19 @@
 import Close from '@assets/icons/close.svg';
+import PencilActive from '@assets/icons/pencil-active.svg';
+import PencilDefault from '@assets/icons/pencil-default.svg';
 import Profle from '@assets/icons/profile.svg';
 import Submit from '@assets/icons/submit.svg';
+import TemplateActive from '@assets/icons/template-active.svg';
+import TemplateDefault from '@assets/icons/template-default.svg';
 
 export const iconFactory = {
+  close: Close,
+  pencilActive: PencilActive,
+  pencilDefault: PencilDefault,
   profile: Profle,
   submit: Submit,
-  close: Close,
+  templateActive: TemplateActive,
+  templateDefault: TemplateDefault,
 };
 
 export type Icons = keyof typeof iconFactory;
