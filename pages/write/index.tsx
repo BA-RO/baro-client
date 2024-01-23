145fe6810f4388b717e69a0242808c72e9131794
원진 <fsdjin159@gmail.com>
2024-01-24 02:04:04 +0900

    [Feature/BAR-165] 끄적이는 레이아웃 작성 & 데이터 구조화 (#38)
    * style: max width 및 전역 설정 추가
    
    * feat: 가이드 컴포넌트 작성
    
    * feat: 데이터 구조화
    
    * refactor: page 스타일 삭제
    
    * feat: 끄적이는 히스토리 dateLabel 스타일링
    
    * feat: dayjs locale 설정
    
    * feat: 히스토리 카드 스타일링
    
    * feat: 컨테이너 스타일링
    
    * refactor: 리뷰 반영
    
    * feat: [WIP] 그리드 레이아웃 수정 필요
    
    * feat: 컬럼 로직 변경
    
    * style: 마크업 디테일

---
+++ pages/write/index.tsx
@@ -0,0 +1,60 @@
+import WriteInput from '@components/Input/WriteInput';
+import WriteGuide from '@domain/Write/components/Guide';
+import WritePageTab from '@domain/Write/components/WritePageTab';
+import WriteHistory from '@domain/Write/History';
+import { type WriteHisotry } from '@domain/Write/types';
+import { useInput } from '@hooks/useInput';
+
+const MOCK: WriteHisotry[] = [
+  {
+    date: '2024-01-03',
+    history: [
+      { createAt: '2024-01-03T01:01', id: '1', value: 'test 1' },
+      { createAt: '2024-01-03T11:01', id: '2', value: 'test 2' },
+      {
+        createAt: '2024-01-03T21:01',
+        id: '11',
+        value:
+          'test 11test 11test 11test 11test 11test 11test 11test 11test 11test 11test 11test 11',
+      },
+      { createAt: '2024-01-03T11:01', id: '21', value: 'test 21' },
+      { createAt: '2024-01-03T21:01', id: '12', value: 'test 12' },
+      { createAt: '2024-01-03T19:01', id: '22', value: 'test 22' },
+      { createAt: '2024-01-03T23:01', id: '23', value: 'test 23' },
+    ],
+  },
+  {
+    date: '2024-01-04',
+    history: [
+      { createAt: '2024-01-04T01:01', id: '3', value: 'test 3' },
+      { createAt: '2024-01-04T01:01', id: '4', value: 'test 4' },
+      { createAt: '2024-01-04T11:01', id: '7', value: 'test 7' },
+    ],
+  },
+  {
+    date: '2024-01-05',
+    history: [
+      { createAt: '2024-01-05T11:01', id: '5', value: 'test 5' },
+      { createAt: '2024-01-05T19:01', id: '6', value: 'test 6' },
+    ],
+  },
+];
+
+const WritePage = () => {
+  const writeInput = useInput({ id: 'write-input' });
+
+  return (
+    <WritePageTab
+      write={
+        <>
+          <WriteGuide />
+          <WriteHistory data={MOCK} />
+          <WriteInput inputProps={writeInput} />
+        </>
+      }
+      template={null}
+    />
+  );
+};
+
+export default WritePage;
---
+++ src/domain/Write/components/Guide/index.tsx
@@ -0,0 +1,11 @@
+import * as styles from './style.css';
+
+const WriteGuide = () => {
+  return (
+    <p className={styles.guideText}>
+      끄적인 글은 일주일 후에 사라져요! 오래 간직하고 싶다면 저장해주세요 😊
+    </p>
+  );
+};
+
+export default WriteGuide;
