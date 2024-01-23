12b5f602fb2217390b9ec67b93ea0c718e7eba8f
cje <chej098t@gmail.com>
2024-01-23 17:45:00 +0900

    refactor: 상수에 as const 키워드 추가

--- src/constants/spellCheck.ts
+++ src/constants/spellCheck.ts
@@ -2,7 +2,7 @@ export const SPELLCHECK_TYPE = {
   space: '띄어쓰기',
   spell: '맞춤법',
   doubt: '표준어의심',
-};
+} as const;
 
 export const SPELLCHECK_STATE_MESSAGE = {
   SUCCESS: '맞춤법 검사가 완료되었어요. 올바른 문장을 확인해보세요!',
