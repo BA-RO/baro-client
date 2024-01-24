8012ae98eb45f904144de6bb51e5830058bdca4f
cje <chej098t@gmail.com>
2024-01-24 17:38:21 +0900

    fix: import문으로 수정

--- pages/api/spell-check.ts
+++ pages/api/spell-check.ts
@@ -1,10 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import hanspell from 'hanspell';
 
 import { HTTP_TIMEOUT } from '@constants/http';
 
 import { type SpellCheckResponse } from '../../src/types/spellCheck';
-
-const hanspell = require('hanspell');
 
 interface SpellCheckResult {
   /* 맞춤법 오류 종류 */
