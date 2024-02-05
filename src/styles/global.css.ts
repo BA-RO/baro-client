e5482ace8a37f8598fde4cf156ea9cf35cc802a4
dongkyun-dev <thd6576@naver.com>
2024-02-05 20:21:35 +0900

    style: global에 설정된 요소 제거

--- src/styles/global.css.ts
+++ src/styles/global.css.ts
@@ -20,9 +20,6 @@ globalStyle('body', {
   lineHeight: 1,
   WebkitFontSmoothing: 'antialiased',
   MozOsxFontSmoothing: 'grayscale',
-  maxWidth: '1680px',
-  padding: '0 120px',
-  margin: '0 auto',
 });
 
 globalStyle('h1, h2, h3, h4, h5, h6, p', {
