8985bc354d8aed395509d210301bbf265d7f10a3
dongkyun-dev <thd6576@naver.com>
2024-02-05 20:21:05 +0900

    style: 반응형 대응을 위한 sprinkle 작성

--- src/styles/sprinkles.css.ts
+++ src/styles/sprinkles.css.ts
@@ -15,6 +15,31 @@ const colorProperties = defineProperties({
   },
 });
 
+const width = {
+  322: '322px',
+  336: '336px',
+  389: '389px',
+  688: '688px',
+  1000: '1000px',
+  1200: '1200px',
+};
+
+const responsiveProperties = defineProperties({
+  conditions: {
+    small: { '@media': 'screen and (min-width: 768px)' },
+    middle: { '@media': 'screen and (min-width: 1024px)' },
+    large: { '@media': 'screen and (min-width: 1440px)' },
+  },
+  defaultCondition: 'large',
+  properties: {
+    width,
+  },
+});
+
 export type Sprinkles = Parameters<typeof sprinkles>[0];
 
-export const sprinkles = createSprinkles(fontProperties, colorProperties);
+export const sprinkles = createSprinkles(
+  fontProperties,
+  colorProperties,
+  responsiveProperties,
+);
