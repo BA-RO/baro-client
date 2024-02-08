4fba2c38a6a58c3c080f0dfe7aac51c1470ae9bf
dongkyun-dev <thd6576@naver.com>
2024-02-08 17:27:21 +0900

    feat: 테스트용도 코드 추가

--- pages/redirect/[authType]/index.tsx
+++ pages/redirect/[authType]/index.tsx
@@ -2,9 +2,8 @@ import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import Cookies from 'js-cookie';
 
-import { getToken } from '@api/auth/auth';
+import { getRenewToken, getToken } from '@api/auth/auth';
 import { type OAuthType } from '@api/auth/types';
-import { ROUTES } from '@constants/routes';
 
 const Bridge = () => {
   const router = useRouter();
@@ -23,11 +22,14 @@ const Bridge = () => {
       localStorage.setItem('accessToken', data.accessToken);
       Cookies.set('refreshToken', data.refreshToken);
 
-      router.events.on('routeChangeStart', (url) => {
-        window.location.href = url;
-      });
+      setTimeout(async () => {
+        await getRenewToken(data.refreshToken);
+      }, 2000);
+      // router.events.on('routeChangeStart', (url) => {
+      //   window.location.href = url;
+      // });
 
-      router.replace(ROUTES.끄적이는);
+      // router.replace(ROUTES.끄적이는);
     })();
   }, [router.query, router]);
 
