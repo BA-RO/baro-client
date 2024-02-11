0b06acd5d82a4db68186b18ecdf59fa424e0257d
dongkyun-dev <thd6576@naver.com>
2024-02-11 19:21:27 +0900

    feat: 기본 모델 작성

--- src/domain/참고하는/types/index.ts
+++ src/domain/참고하는/types/index.ts
@@ -1,10 +1,14 @@
-import { type CATEGORY } from '@domain/참고하는/models';
+import { type CATEGORY, type FILTER_BUTTONS } from '@domain/참고하는/models';
+
+export type Category = keyof typeof CATEGORY;
 
 export interface Refer {
   templateId: number;
-  category: keyof typeof CATEGORY;
+  category: Category;
   subCategory: string;
   content: string;
   savedCount: number;
   copiedCount: number;
 }
+
+export type FilterButton = keyof typeof FILTER_BUTTONS;
