b3fd50900fa1610d9a6b08268ec023c2c6d727f2
wonjin-dev <fsdjin159@gmail.com>
2024-02-11 15:42:45 +0900

    feat: 복사 기능 구현

--- src/domain/끄적이는/History/components/Card/index.tsx
+++ src/domain/끄적이는/History/components/Card/index.tsx
@@ -2,11 +2,20 @@ import dayjs from 'dayjs';
 
 import Icon from '@components/Icon';
 import { type Write } from '@domain/끄적이는/types';
+import { useToastStore } from '@stores/toast';
 import { COLORS } from '@styles/tokens';
 
 import * as styles from './style.css';
 
 const WriteHistoryCard = ({ createAt, value }: Omit<Write, 'id'>) => {
+  const { showToast } = useToastStore();
+  const handleCopyClick = () => {
+    navigator.clipboard.writeText(value);
+    showToast({
+      message: '문장이 복사되었어요. 원하는 곳에 붙여넣기(Ctrl+V)를 해주세요!',
+    });
+  };
+
   return (
     <li className={styles.container}>
       <div className={styles.header}>
@@ -14,7 +23,7 @@ const WriteHistoryCard = ({ createAt, value }: Omit<Write, 'id'>) => {
           {dayjs(createAt).locale('ko').format('a h:mm')}
         </p>
         <div className={styles.ctaWrapper}>
-          <button>
+          <button onClick={handleCopyClick}>
             <Icon
               icon={'copy'}
               width={24}
