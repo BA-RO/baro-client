6c741674ca7e6f48c87bd9029637510d88deeaf5
cje <chej098t@gmail.com>
2024-02-12 19:56:54 +0900

    refactor(Header): Dropdown 컴포넌트 적용

--- src/components/Layout/components/Header.tsx
+++ src/components/Layout/components/Header.tsx
@@ -1,49 +1,27 @@
 import Link from 'next/link';
-import { type MouseEvent, useState } from 'react';
 
 import Logo from '@assets/images/logo.svg';
 import { type HeaderType } from '@models/layout';
 
 import * as styles from '../style.css';
 import LoginButton from './LoginButton';
-import ProfileButton from './ProfileButton';
-import ProfileDialog from './ProfileDialog';
+import UserButton from './UserButton';
 
 interface HeaderProps {
   type: HeaderType;
 }
 
 const Header = ({ type }: HeaderProps) => {
-  const [showProfileDialog, setShowProfileDialog] = useState(false);
-
-  const handleProfileClick = (e: MouseEvent<HTMLButtonElement>) => {
-    e.stopPropagation();
-    setShowProfileDialog(!showProfileDialog);
-  };
-
-  const HeaderButton = () =>
-    type === 'intro' ? (
-      <LoginButton />
-    ) : (
-      <ProfileButton handleProfileClick={handleProfileClick} />
-    );
+  const HeaderButtonGroup = () =>
+    type === 'intro' ? <LoginButton /> : <UserButton />;
 
   return (
-    <>
-      <header className={styles.headerWrapper}>
-        <div className={styles.logo}>
-          <Link href="/">
-            <Logo />
-          </Link>
-        </div>
-        <div className={styles.buttonsWrapper({ type })}>
-          <HeaderButton />
-        </div>
-      </header>
-      {showProfileDialog && (
-        <ProfileDialog closeDialog={() => setShowProfileDialog(false)} />
-      )}
-    </>
+    <header className={styles.header}>
+      <Link href="/" className={styles.logo}>
+        <Logo />
+      </Link>
+      <HeaderButtonGroup />
+    </header>
   );
 };
 
