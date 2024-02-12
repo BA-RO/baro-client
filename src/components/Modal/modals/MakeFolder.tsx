35bd7b1b35a11fb3df7b86cc5b34c983878b8253
dongkyun-dev <thd6576@naver.com>
2024-02-12 16:38:50 +0900

    feat: 새폴더만들기 모달 작성

---
+++ src/components/Modal/modals/MakeFolder.tsx
@@ -0,0 +1,98 @@
+import { type ChangeEvent, useState } from 'react';
+import { useQueryClient } from '@tanstack/react-query';
+import { assignInlineVars } from '@vanilla-extract/dynamic';
+import { AxiosError } from 'axios';
+import clsx from 'clsx';
+
+import Button from '@components/Button';
+import Icon from '@components/Icon';
+import ModalContainer from '@components/Modal/components/ModalContainer';
+import * as styles from '@components/Modal/style.css';
+import usePostMemoFolders from '@queries/usePostMemoFolders';
+import { useModalStore } from '@stores/modal';
+import { COLORS } from '@styles/tokens';
+
+const MakeFolder = () => {
+  const queryClient = useQueryClient();
+
+  const { closeModal } = useModalStore();
+  const [value, setValue] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const { mutateAsync } = usePostMemoFolders();
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setErrorMessage('');
+    setValue(e.target.value);
+  };
+
+  const handle만들기Click = async () => {
+    if (value.length > 10) return setErrorMessage('10자 내로 입력해주세요!');
+
+    try {
+      await mutateAsync(value);
+      await queryClient.invalidateQueries({
+        queryKey: ['memo-folders'],
+      });
+      closeModal();
+    } catch (e) {
+      if (!(e instanceof AxiosError) || !e.response) throw e;
+      if (e.response.data.errorCode === 'MF01')
+        return setErrorMessage('이미 사용 중인 폴더 이름이에요!');
+      throw e;
+    }
+  };
+
+  return (
+    <ModalContainer>
+      <strong className={styles.makeFolderTitle}>새 폴더 만들기</strong>
+      <label htmlFor="makeFolder" className={styles.makeFolderDescription}>
+        폴더 이름
+      </label>
+      <div className={styles.makeFolderInputWrapper}>
+        <input
+          id="makeFolder"
+          className={clsx(
+            styles.makeFolderInput,
+            errorMessage && styles.errorInput,
+          )}
+          placeholder="새 폴더 이름을 입력해주세요 (ex. 축하)"
+          onChange={handleInputChange}
+        />
+      </div>
+      {errorMessage && (
+        <span className={styles.errorMessage}>
+          <div className={styles.errorIcon}>
+            <Icon width={20} height={20} icon="error" />
+          </div>
+          {errorMessage}
+        </span>
+      )}
+      <div className={styles.makeFolderButtonWrapper}>
+        <Button
+          className={styles.button}
+          style={assignInlineVars({
+            [styles.buttonColor]: COLORS['Grey/600'],
+            [styles.buttonBackgroundColor]: COLORS['Grey/150'],
+          })}
+          onClick={closeModal}
+        >
+          취소
+        </Button>
+        <Button
+          className={clsx(styles.button, !value && styles.buttonDisabled)}
+          disabled={!value}
+          style={assignInlineVars({
+            [styles.buttonColor]: COLORS['Grey/White'],
+            [styles.buttonBackgroundColor]: COLORS['Blue/Default'],
+          })}
+          onClick={handle만들기Click}
+        >
+          만들기
+        </Button>
+      </div>
+    </ModalContainer>
+  );
+};
+
+export default MakeFolder;
