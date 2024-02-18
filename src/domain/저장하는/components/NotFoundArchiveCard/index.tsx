5136043fe9bcc4b5ea969376e495f9206c0b523a
cje <chej098t@gmail.com>
2024-02-18 13:47:23 +0900

    feat(NotFoundArchiveCard): 저장된 템플릿이 없을 경우, 메인으로 이동

--- src/domain/저장하는/components/NotFoundArchiveCard/index.tsx
+++ src/domain/저장하는/components/NotFoundArchiveCard/index.tsx
@@ -1,9 +1,14 @@
+import { useRouter } from 'next/router';
+
 import Empty from '@assets/images/empty.svg';
 import Button from '@components/Button';
+import { ROUTES } from '@constants/routes';
 
 import * as styles from './style.css';
 
 const NotFoundArchiveCard = () => {
+  const router = useRouter();
+
   return (
     <div className={styles.notFound}>
       <Empty />
@@ -13,7 +18,7 @@ const NotFoundArchiveCard = () => {
           자주 사용하는 글과 템플릿을 저장해보세요.
         </span>
       </div>
-      <Button state="enabled" size="M">
+      <Button state="enabled" size="M" onClick={() => router.push(ROUTES.MAIN)}>
         문장 템플릿 보러가기
       </Button>
     </div>
