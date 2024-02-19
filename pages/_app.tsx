91081a8beb735acfe46e7ef8fc9409de508552b6
dongkyun-dev <thd6576@naver.com>
2024-02-20 00:40:01 +0900

    feat: meta 태그 제거

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,23 +13,6 @@ const App = ({ Component, pageProps }: AppProps) => {
     <TanstackQueryProvider dehydratedState={pageProps.dehydratedState}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
         <meta name="description" content="baro 설명" />
         <meta name="keyword" content="baro 바로" />
         <meta property="og:type" content="website" />
