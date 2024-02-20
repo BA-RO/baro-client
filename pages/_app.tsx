20b2f36b85a7ebea789e4d874cf7534f79e77ab4
장동균 <57230590+dongkyun-dev@users.noreply.github.com>
2024-02-21 00:09:35 +0900

    [Feature/BAR-242] head 태그 추가 (#72)
    * feat: head 내용 추가
    
    * feat: test
    
    * feat: 스킴 분기 추가
    
    * feat: 스킴 분기 제거
    
    * feat: 이미지 경로 수정
    
    * feat: meta 태그 제거
    
    * feat: og image 추가
    
    * feat: og image 추가
    
    * feat: og image 추가
    
    * feat: og image 추가
    
    * feat: og image 추가
    
    * feat: 불필요 import 제거
    
    * feat: ogimage url 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import 'react-notion-x/src/styles.css';
 import '@styles/global.css';
@@ -10,6 +11,21 @@ import Toast from '@components/Toast';
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <TanstackQueryProvider dehydratedState={pageProps.dehydratedState}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="바로 잡아 바로 쓰는, 바로" />
+        <meta name="keyword" content="baro 바로" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://ba-ro.co.kr" />
+        <meta property="og:title" content="바로" />
+        <meta
+          property="og:image"
+          content="https://image.ba-ro.co.kr/ba-ro.png"
+        />
+        <meta property="og:description" content="바로 잡아 바로 쓰는, 바로" />
+        <meta property="og:site_name" content="baro" />
+        <title>바로 잡아 바로 쓰는, 바로</title>
+      </Head>
       <Component {...pageProps} />
       <Toast />
       <Modal />
