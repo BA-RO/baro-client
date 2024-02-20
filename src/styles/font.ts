db9336909cff147ef19e7b3b0dece55b5a769014
dongkyun-dev <thd6576@naver.com>
2024-02-21 01:37:34 +0900

    feat: 폰트 추가

--- src/styles/font.ts
+++ src/styles/font.ts
@@ -22,3 +22,25 @@ export const pretendard = localFont({
     'sans-serif',
   ],
 });
+
+export const gmarket = localFont({
+  src: [
+    {
+      path: '../../public/fonts/GmarketSansLight.otf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/GmarketSansMedium.otf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/GmarketSansBold.otf',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  display: 'swap',
+  preload: true,
+});
